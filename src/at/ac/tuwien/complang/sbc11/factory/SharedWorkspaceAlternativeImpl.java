37b9c792d8689cf9d96408cd2029888ae24bb85f
Gerhard Schraml <g.schraml@gmx.at>
2011-11-22 00:53:12 +0100

    removed transaction methods from alternative impl stub

--- src/at/ac/tuwien/complang/sbc11/factory/SharedWorkspaceAlternativeImpl.java
+++ src/at/ac/tuwien/complang/sbc11/factory/SharedWorkspaceAlternativeImpl.java
@@ -91,22 +91,4 @@ public class SharedWorkspaceAlternativeImpl extends SharedWorkspace {
 		return null;
 	}
 
-	@Override
-	public void startTransaction() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void commitTransaction() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void rollbackTransaction() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
