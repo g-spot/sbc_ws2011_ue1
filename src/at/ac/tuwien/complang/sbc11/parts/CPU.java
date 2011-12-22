73bd211b4a0a88d695ca358f68cc462918086459
Gerhard Schraml <g.schraml@gmx.at>
2011-12-22 10:36:01 +0100

    added computer types

--- src/at/ac/tuwien/complang/sbc11/parts/CPU.java
+++ src/at/ac/tuwien/complang/sbc11/parts/CPU.java
@@ -1,10 +1,24 @@
 package at.ac.tuwien.complang.sbc11.parts;
 
+import org.mozartspaces.capi3.Index;
 import org.mozartspaces.capi3.Queryable;
 
 @Queryable
 public class CPU extends Part {
 
 	private static final long serialVersionUID = 1331527406898166484L;
+	
+	public enum CPUType { SINGLE_CORE, DUAL_CORE, QUAD_CORE };
+	
+	@Index
+	private CPUType cpuType;
+
+	public CPUType getCpuType() {
+		return cpuType;
+	}
+
+	public void setCpuType(CPUType cpuType) {
+		this.cpuType = cpuType;
+	}
 
 }
